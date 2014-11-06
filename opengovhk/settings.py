2db51396aed5e5f336c974aef21831a33a76d411
Guy Freeman <git@gfrm.in>
2014-11-06 15:22:16 +0800

    Static fix

--- opengovhk/settings.py
+++ opengovhk/settings.py
@@ -83,12 +83,12 @@ class Production(OpenGovHK):
     ALLOWED_HOSTS = ['opengov.hk']
     ALLOWED_HOSTS = ['*'] # temp flexibility
 
-    PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_PATH, 'static'),
-    )
+#    PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+#    STATICFILES_DIRS = (
+#        os.path.join(PROJECT_PATH, 'static'),
+#    )
 
-    STATIC_URL = '/static/'   
+#    STATIC_URL = '/static/'   
 
     PROJECT_ROOT = os.path.abspath(os.path.dirname(__name__))
     STATIC_ROOT = os.path.abspath(os.path.join(PROJECT_ROOT, 'static'))
